68b33116594a8aa7ed2ac99be03bbe393e813e8e
alesantuz <alessandro.santuz@gmail.com>
2021-06-28 14:40:54 +0200

    Section 4 bug fix

--- muscle_synergies.R
+++ muscle_synergies.R
@@ -1850,58 +1850,63 @@ for (condition in conditions) {
         data_P <- data_P[lapply(data_P, length)>0]
         data_M <- data_M[lapply(data_M, length)>0]
         
-        # Put primitives in a single data frame
-        data_P <- plyr::ldply(data_P, data.frame, .id="trial")
-        # Put modules in a single data frame
-        data_M <- plyr::ldply(data_M, data.frame, .id="trial")
-        
-        data_P_av <- data.frame(time=c(1:(ncol(data_P)-1)),
-                                value=colMeans(data_P[, -1]))
-        data_P_sd <- data.frame(time=c(1:(ncol(data_P)-1)),
-                                ymin=data_P_av$value-apply(data_P[, -1], 2, sd),
-                                ymax=data_P_av$value+apply(data_P[, -1], 2, sd))
-        
-        data_M_av <- data.frame(value=colMeans(data_M[, -1]))
-        data_M_av <- data.frame(muscle=rownames(data_M_av),
-                                data_M_av)
-        
-        data_P <- reshape2::melt(data_P, id="trial")
-        data_M <- reshape2::melt(data_M, id="trial")
-        
-        temp_P <- ggplot2::ggplot() +
-            ggplot2::ggtitle(paste0("Motor primitive ", syn)) +
-            ggplot2::ylim(-0.2, 1.2) +
-            ggplot2::geom_ribbon(data=data_P_sd,
-                                 ggplot2::aes(x=time, ymin=ymin, ymax=ymax),
-                                 fill="grey80") +
-            ggplot2::geom_line(data=data_P_av,
-                               ggplot2::aes(x=time, y=value),
-                               colour=c_signal, size=s_line) +
-            ggplot2::theme(axis.title=ggplot2::element_blank(),
-                           panel.background=ggplot2::element_rect(fill=c_back, colour=c_bord),
-                           panel.grid.major=ggplot2::element_line(colour=c_min, size=s_min),
-                           panel.grid.minor=ggplot2::element_blank(),
-                           legend.position="none")
-        
-        temp_M <- ggplot2::ggplot() +
-            ggplot2::ggtitle(paste0("Motor module ", syn)) +
-            ggplot2::ylim(0, 1) +
-            ggplot2::geom_hline(yintercept=c(0.25, 0.5, 0.75, 1), size=s_min, color=c_thin) +
-            ggplot2::geom_bar(data=data_M_av,
-                              ggplot2::aes(x=muscle, y=value),
-                              fill=c_bars, alpha=0.3,
-                              stat="identity") +
-            ggplot2::scale_x_discrete(limits=data_M_av$muscle) +
-            ggplot2::geom_jitter(data=data_M,
-                                 ggplot2::aes(x=variable, y=value),
-                                 fill=c_bars, width=0.1, size=0.1) +
-            ggplot2::theme(axis.title=ggplot2::element_blank(),
-                           axis.text.y=ggplot2::element_blank(),
-                           panel.background=ggplot2::element_rect(fill=c_back, colour=c_bord),
-                           panel.grid.major=ggplot2::element_blank(),
-                           panel.grid.minor=ggplot2::element_blank(),
-                           axis.ticks=ggplot2::element_blank(),
-                           legend.position="none")
+        if (length(data_P)>0) {
+            # Put primitives in a single data frame
+            data_P <- plyr::ldply(data_P, data.frame, .id="trial")
+            # Put modules in a single data frame
+            data_M <- plyr::ldply(data_M, data.frame, .id="trial")
+            
+            data_P_av <- data.frame(time=c(1:(ncol(data_P)-1)),
+                                    value=colMeans(data_P[, -1]))
+            data_P_sd <- data.frame(time=c(1:(ncol(data_P)-1)),
+                                    ymin=data_P_av$value-apply(data_P[, -1], 2, sd),
+                                    ymax=data_P_av$value+apply(data_P[, -1], 2, sd))
+            
+            data_M_av <- data.frame(value=colMeans(data_M[, -1]))
+            data_M_av <- data.frame(muscle=rownames(data_M_av),
+                                    data_M_av)
+            
+            data_P <- reshape2::melt(data_P, id="trial")
+            data_M <- reshape2::melt(data_M, id="trial")
+            
+            temp_P <- ggplot2::ggplot() +
+                ggplot2::ggtitle(paste0("Motor primitive ", syn)) +
+                ggplot2::ylim(-0.2, 1.2) +
+                ggplot2::geom_ribbon(data=data_P_sd,
+                                     ggplot2::aes(x=time, ymin=ymin, ymax=ymax),
+                                     fill="grey80") +
+                ggplot2::geom_line(data=data_P_av,
+                                   ggplot2::aes(x=time, y=value),
+                                   colour=c_signal, size=s_line) +
+                ggplot2::theme(axis.title=ggplot2::element_blank(),
+                               panel.background=ggplot2::element_rect(fill=c_back, colour=c_bord),
+                               panel.grid.major=ggplot2::element_line(colour=c_min, size=s_min),
+                               panel.grid.minor=ggplot2::element_blank(),
+                               legend.position="none")
+            
+            temp_M <- ggplot2::ggplot() +
+                ggplot2::ggtitle(paste0("Motor module ", syn)) +
+                ggplot2::ylim(0, 1) +
+                ggplot2::geom_hline(yintercept=c(0.25, 0.5, 0.75, 1), size=s_min, color=c_thin) +
+                ggplot2::geom_bar(data=data_M_av,
+                                  ggplot2::aes(x=muscle, y=value),
+                                  fill=c_bars, alpha=0.3,
+                                  stat="identity") +
+                ggplot2::scale_x_discrete(limits=data_M_av$muscle) +
+                ggplot2::geom_jitter(data=data_M,
+                                     ggplot2::aes(x=variable, y=value),
+                                     fill=c_bars, width=0.1, size=0.1) +
+                ggplot2::theme(axis.title=ggplot2::element_blank(),
+                               axis.text.y=ggplot2::element_blank(),
+                               panel.background=ggplot2::element_rect(fill=c_back, colour=c_bord),
+                               panel.grid.major=ggplot2::element_blank(),
+                               panel.grid.minor=ggplot2::element_blank(),
+                               axis.ticks=ggplot2::element_blank(),
+                               legend.position="none")
+        } else {
+            temp_P <- NULL
+            temp_M <- NULL
+        }
         
         varname_P <- paste0("r", 2*syn)
         varname_M <- paste0("r", 2*syn-1)
