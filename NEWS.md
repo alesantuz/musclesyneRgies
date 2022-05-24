72bcbf1360334c7175d551652a559437babb1f32
alesantuz <alessandro.santuz@gmail.com>
2022-05-24 14:32:51 +0200

    Updated tests

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,15 @@
+# musclesyneRgies 1.1.3.9004 (development version)
+### How to install
+```
+install.packages("devtools")
+devtools::install_github("alesantuz/musclesyneRgies")
+```
+### How to use
+README and vignettes are available both on [CRAN](https://CRAN.R-project.org/package=musclesyneRgies) and on [GitHub](https://github.com/alesantuz/musclesyneRgies).
+
+### What's changed
+- Updated tests to check for compatibility with single-column cycles.
+
 # musclesyneRgies 1.1.3.9003 (development version)
 ### How to install
 ```
--- tests/testthat/test-EMG.R
+++ tests/testthat/test-EMG.R
@@ -1,11 +1,20 @@
-test_that("EMG-related functions produce correct class and output", {
+test_that("EMG-related functions produce correct class and output
+          and independent on number of columns in cycles", {
   data("RAW_DATA")
 
   data1 <- lapply(RAW_DATA, function(x) subsetEMG(x, cy_max = 3))
   data2 <- lapply(data1, filtEMG)
   data3 <- lapply(data2, function(x) normEMG(x, cycle_div = c(100, 100)))
+  data4 <- lapply(RAW_DATA, function(x) {
+    x$cycles <- x$cycles$V1
+    return(x)
+  }) |>
+    lapply(function(x) subsetEMG(x, cy_max = 3)) |>
+    lapply(filtEMG) |>
+    lapply(function(x) normEMG(x, cycle_div = 100))
 
   expect_s3_class(data1[[1]], "EMG")
   expect_s3_class(data2[[1]], "EMG")
   expect_s3_class(data3[[1]], "data.frame")
+  expect_s3_class(data4[[1]], "data.frame")
 })
